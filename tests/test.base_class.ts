b12a51855ae93e8abb23ae80527772074315d817
elhombre <elhombre.dev@gmail.com>
2020-02-19 23:19:26 +0500

    Add test for methods BaseClass.performUpdates()

--- tests/test.base_class.ts
+++ tests/test.base_class.ts
@@ -11,3 +11,12 @@ test('@ClassName() decorator', () => {
   expect(c.className).toBe('TestClassName')
   expect(d.className).toBeUndefined()
 })
+
+test('performUpdates', () => {
+  const c = new TestClass()
+  expect(c.isUpdating).toBeFalsy()
+  c.performUpdates(() => {
+    expect(c.isUpdating).toBeTruthy()
+  })
+  expect(c.isUpdating).toBeFalsy()
+})
